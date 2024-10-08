365f6dba000ef284487ede210aebc07a2d192eb7
Liam O'Connor <liamj.oc@gmail.com>
2024-10-08 11:03:03 +0100

    Add first failing tests

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -5,6 +5,7 @@ import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
 
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -21,7 +22,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Welcome to the Masters 2025")
     }
 
     "render the index page from the application" in {
@@ -30,7 +31,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Welcome to the Masters 2025")
     }
 
     "render the index page from the router" in {
@@ -39,7 +40,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Welcome to the Masters 2025")
     }
   }
 }
